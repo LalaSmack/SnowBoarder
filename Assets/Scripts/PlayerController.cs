804870ee775356d799ac285d655429f7494b2703
Layal Singh <layal.singhkolkata@gmail.com>
2025-07-01 20:12:41 -0400

    Speed Boost

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,38 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
     [SerializeField] ParticleSystem snowParticleSystem; // Reference to the snow particle system
+    [SerializeField] float speed = 15f; // Speed of the player
+    [SerializeField] float boostSpeed = 50f; // Speed when boosted
+    SurfaceEffector2D surfaceEffector2D; // Reference to the SurfaceEffector2D component
     Rigidbody2D rb2d;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindAnyObjectByType<SurfaceEffector2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed; // Increase the speed of the surface effector
+        }
+        else 
+        {
+            surfaceEffector2D.speed = speed; // Decrease the speed of the surface effector
+        }
+    }
+    private void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -26,7 +48,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnCollisionExit2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Ground"))
         {
