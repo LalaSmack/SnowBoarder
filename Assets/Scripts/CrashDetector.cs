ee916e02afeb6e33892d2dfc7c1484b014f1c815
Layal Singh <layal.singhkolkata@gmail.com>
2025-07-01 20:40:53 -0400

    adjusted controlls for post-crash scenario

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,12 +6,20 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] float timeToReload = 0.5f; // Time in seconds before the scene reloads
     [SerializeField] ParticleSystem crashEffect; // Particle effect to play on crash
     [SerializeField] AudioClip crashSound; // Sound effect to play on crash
+    bool isCrashed = false; // Flag to check if the player has crashed
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Ground"))
+        if (other.CompareTag("Ground") )
         {
+            FindAnyObjectByType<PlayerController>().DisableControls(); // Disable player controls
+            if (!isCrashed) // Check if the player has not already crashed
+            {
+                isCrashed = true; // Set the crash flag to true
+                GetComponent<AudioSource>().PlayOneShot(crashSound); // Play the crash sound
+                
+            }
             crashEffect.Play(); // Play the crash effect
-            GetComponent<AudioSource>().PlayOneShot(crashSound); // Play the crash sound
+            
             Invoke("ReloadScene", timeToReload); // Call ReloadScene after a delay
         }
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float boostSpeed = 50f; // Speed when boosted
     SurfaceEffector2D surfaceEffector2D; // Reference to the SurfaceEffector2D component
     Rigidbody2D rb2d;
+    bool canMove = true; // Flag to check if the player can move
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,17 +22,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RotatePlayer();
-        RespondToBoost();
+        if (canMove)
+        {
+            RotatePlayer();
+            RespondToBoost(); 
+        }
+       
     }
 
+    public void DisableControls()
+    {
+        // Disable player controls by setting the speed to 0
+        surfaceEffector2D.speed = 0f;
+        canMove = false; // Set the flag to false to stop player movement
+    }
     private void RespondToBoost()
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
             surfaceEffector2D.speed = boostSpeed; // Increase the speed of the surface effector
         }
-        else 
+        else
         {
             surfaceEffector2D.speed = speed; // Decrease the speed of the surface effector
         }
